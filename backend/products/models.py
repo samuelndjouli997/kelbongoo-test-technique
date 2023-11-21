aa01514b58bc8dca12b9646ff6c4e7c29a691a07
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 00:35:55 +0100

    added models for products

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price_excluding_tax = models.DecimalField(max_digits=8, decimal_places=2)
+    TVA_CHOICES = [
+        (5.5, '5.5%'),
+        (20, '20%'),
+    ]
+    tva = models.FloatField(choices=TVA_CHOICES)
+    ordered_stock = models.PositiveIntegerField()
+    max_available_stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
