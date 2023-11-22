24ae738ccc557ca79036909dbc9b0b21eeb00992
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 01:46:17 +0100

    created UserCart component

---
+++ frontend/src/UserCart.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from './Layout'
+
+const UserCart = () => {
+    
+  return (
+    <Layout>
+        UserCart
+    </Layout>
+  )
+}
+
+export default UserCart
