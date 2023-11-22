4ab7499e665d1cd09c233af55380e99d9c0f64d8
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 10:00:44 +0100

    added colors variables in tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-green': '#075244',
+        'primary-dark-green': '#002927',
+        'primary-orange': '#FEF4D4',
+        'primary-dark-orange': '#f8ab1c',
+      },
+    },
   },
   plugins: [],
 }
