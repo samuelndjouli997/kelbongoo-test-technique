fdbd6d888dda653977269e8a8a48494d35bec858
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 16:18:24 +0100

    imort CartProvider

--- frontend/src/components/CartModal.tsx
+++ frontend/src/components/CartModal.tsx
@@ -1,67 +1,72 @@
-import React from 'react'
-import { IoMdClose } from "react-icons/io";
+import React from 'react';
+import { IoMdClose } from 'react-icons/io';
 import Button from './Button';
 import { Link } from 'react-router-dom';
+import { useCart } from '../context/CartProvider';
 
 interface CartModalProps {
-    onClose: () => void;
-    cartModalOpen: boolean;
+  onClose: () => void;
+  cartModalOpen: boolean;
 }
 
 const CartModal = ({ onClose, cartModalOpen }: CartModalProps) => {
-    
-    return (
-        <>
-        {
-            cartModalOpen && ( <div className="absolute top-14 right-4 flex items-center justify-center">
-                                <div className="bg-white p-4 w-96 rounded-lg z-20">
-                                    <div className="flex justify-end items-center mb-4">
-                                        <button className="text-gray-500" onClick={onClose}>
-                                            <IoMdClose />
-                                        </button>
-                                    </div>
-                        
-                                    <ul className="divide-y divide-gray-600">
-                                            <li className="py-2">
-                                            <div className="flex items-center space-x-4">
-                                                <img
-                                                src=""
-                                                alt="test"
-                                                className="w-14 h-auto object-cover rounded"
-                                                />
-                                                <div>
-                                                <p className="text-sm text-black font-semibold">
-                                                    Test
-                                                </p>
-                                                <p className="text-gray-500 text-xs">
-                                                    Prix: 0€
-                                                </p>
-                                                <p className="text-gray-500 text-xs">
-                                                    Quantité: 0
-                                                </p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                        
-                                    <div className="mt-4 flex justify-between items-center">
-                                        <span className="text-sm font-semibold text-gray-500">Total:</span>
-                                        <span className="text-lg font-bold"></span>
-                                    </div>
-                        
-                                    <Button
-                                        onClick={onClose}
-                                    >
-                                        <Link to="/cart">Voir le panier</Link>
-                                    </Button>
-                                </div>
-                            </div> 
-                        )
+  const { state } = useCart();
 
-        }
-        </>
-      
-    )
-  };
+  return (
+    <>
+      {cartModalOpen && (
+        <div className="absolute top-14 right-4 flex items-center justify-center">
+          <div className="bg-white p-4 w-96 rounded-lg z-20 shadow-md">
+            <div className="flex justify-end items-center mb-4">
+              <button className="text-gray-500" onClick={onClose}>
+                <IoMdClose />
+              </button>
+            </div>
 
-export default CartModal
+            {state.cart.length === 0 ? (
+              <p className="text-center text-gray-500 text-[16px] mb-6">Le panier est vide 😢.</p>
+            ) : (
+              <>
+                <ul className="divide-y divide-gray-600">
+                  {state.cart.map((cartItem, index) => (
+                    <li key={index} className="py-2">
+                      <div className="flex items-center space-x-4">
+                        <img
+                          src={cartItem.product?.image}
+                          alt={cartItem.product?.name}
+                          className="w-14 h-auto object-cover rounded"
+                        />
+                        <div>
+                          <p className="text-sm text-black font-semibold">
+                            {cartItem.product?.name}
+                          </p>
+                          <p className="text-gray-500 text-xs">
+                            Prix: {cartItem.product?.price_excluding_tax}€
+                          </p>
+                          <p className="text-gray-500 text-xs">
+                            Quantité: {cartItem?.quantity}
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+
+                <div className="mt-4 flex justify-between items-center">
+                  <span className="text-sm font-semibold text-gray-500">Total:</span>
+                  <span className="text-lg font-bold"></span>
+                </div>
+
+                <Button onClick={onClose}>
+                  <Link to="/cart">Voir le panier</Link>
+                </Button>
+                </>
+            )}
+            </div>
+        </div>
+        )}
+    </>
+    );
+}
+
+export default CartModal;
