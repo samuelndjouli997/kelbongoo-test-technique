ce28b4324bac71d24c9cc5db96e754ae8cdf90bb
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 01:57:18 +0100

    imported Layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import UserCart from './UserCart.tsx';
+import Layout from './Layout.tsx';
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Layout>
+      <RouterProvider router={router} />
+    </Layout>
   </React.StrictMode>,
 )
