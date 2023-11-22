8af7d77358badd1b280dd10f8c6aef8877c9aaee
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 16:16:01 +0100

    implemented UserCart

--- frontend/src/UserCart.tsx
+++ frontend/src/UserCart.tsx
@@ -1,13 +1,55 @@
-import React from 'react'
-import Layout from './Layout'
+import React from 'react';
+import Layout from './Layout';
+import { FaShoppingCart } from 'react-icons/fa';
+import Button from './components/Button';
+import { useCart } from './context/CartProvider';
 
 const UserCart = () => {
-    
+  const { state } = useCart();
+
   return (
     <Layout>
-        <h1>UserCart</h1>
-    </Layout>
-  )
-}
+      <section className="container mx-auto px-4 py-10 lg:px-12 lg:pt-16 lg:pb-20">
+        <div className="flex justify-center mb-10 items-center space-x-3 text-primary-dark-green font-semibold text-2xl">
+          <FaShoppingCart />
+          <h2>Mon panier</h2>
+        </div>
 
-export default UserCart
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr>
+                <th className="py-2 px-4 border-b">Quantité</th>
+                <th className="py-2 px-4 border-b">Produit</th>
+                <th className="py-2 px-4 border-b">Prix</th>
+              </tr>
+            </thead>
+            <tbody>
+              {state.cart.map((cartItem, index) => (
+                <tr key={index}>
+                  <td className="py-2 px-4 border-b">{cartItem?.quantity}</td>
+                  <td className="py-2 px-4 border-b">{cartItem.product?.name}</td>
+                  <td className="py-2 px-4 border-b">{cartItem.product?.price_excluding_tax}€</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        {/* Total */}
+        <div className="flex justify-end items-center my-4 space-x-3 text-primary-dark-green font-semibold text-2xl">
+          <h2>Total:</h2>
+          <h2>0€</h2>
+        </div>
+
+        <div className="text-center my-4">
+          <Button>
+            Commander
+          </Button>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+
+export default UserCart;
