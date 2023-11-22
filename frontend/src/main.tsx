128364d1d723875cbfdfd6dfb9d379169ca48173
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 01:49:54 +0100

    added react router to handle routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import UserCart from './UserCart.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <App />
+    ),
+  },
+  {
+    path: "cart",
+    element: <UserCart />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
