86d124a34b7dbb5e20c7739f53d26117bf8b72bc
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 10:55:52 +0100

    created a function to fetchData et to get products

--- frontend/src/components/Products.tsx
+++ frontend/src/components/Products.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { getProducts } from '../utils/getProducts';
+import ProductComponent from './ProductComponent';
+import { Product } from '../types/types';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const productsData = await getProducts();
+        setProducts(productsData);
+      } catch (error) {
+        // Gérer les erreurs de manière appropriée, par exemple, afficher un message à l'utilisateur
+        console.error('Error getting products:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+    // console.log(products);
+
+    // here we create an async function to post the items to the cart
+        
 
     return (
-        <>Products</>
+        <div className="mx-auto grid lg:grid-cols-4 lg:gap-y-3 lg:gap-x-1 shadow-md mb-4 lg:px-12 lg:py-20">
+            {
+                products.map((product:Product) => (
+                    <ProductComponent
+                        key={product.id}
+                        product={product}
+                    />
+                ))
+            }
+        </div>
       );
 }
 
