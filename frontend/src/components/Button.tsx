e70aeb83067cedf86412ce69158f35ba8ba3d28a
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-22 11:15:52 +0100

    added a new props onClick

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 interface ButtonProps {
     children: React.ReactNode;
+    onClick?: () => void;
 }
 
-const Button = ({children}: ButtonProps) => {
+const Button = ({children, onClick}: ButtonProps) => {
     return (
-        <button className="px-4 py-2 rounded bg-primary-green text-primary-orange hover:bg-primary-dark-green hover:text-primary-dark-orange transition-colors duration-300">
+        <button onClick={onClick} className="px-4 py-2 rounded bg-primary-green text-primary-orange hover:bg-primary-dark-green hover:text-primary-dark-orange transition-colors duration-300">
             {children}
         </button>
     )
