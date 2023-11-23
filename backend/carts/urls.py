caf21976e463bbcf9bab4beb13f32428d0c36f23
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 20:31:35 +0100

    added a new path called checkout

--- backend/carts/urls.py
+++ backend/carts/urls.py
@@ -1,7 +1,8 @@
 # cart/urls.py
 from django.urls import path
-from .views import update_cart
+from .views import update_cart, checkout
 
 urlpatterns = [
     path('update_cart/', update_cart, name='update_cart'),
+    path('checkout/', checkout, name='checkout'),
 ]
