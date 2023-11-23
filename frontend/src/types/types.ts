2c9879e081e593da550ccdc1b6485da607a0bd2b
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 01:13:02 +0100

    added tva type, and modified price_excluding_tax type to number

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,7 +2,8 @@ export interface Product {
     id: number;
     image: string;
     name: string;
-    price_excluding_tax: string;
+    price_excluding_tax: number;
+    tva: number;
     max_available_stock: number;
     ordered_stock: number;
   }
