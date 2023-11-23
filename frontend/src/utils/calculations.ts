3c1230be77b1ada5e8f66ee8e05272f93aa8b32d
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 13:41:08 +0100

    created a function named calculateTotalTTC to calculate the total TTC

--- frontend/src/utils/calculations.ts
+++ frontend/src/utils/calculations.ts
@@ -4,3 +4,18 @@ export const calculatePriceTTC = (priceHT: number, tva: number) => {
         // We use toFixed() to round the price to 2 decimal places
         return Number(priceTTC.toFixed(2));
   };
+
+// Function to calculate the total TTC for an array of cart items
+export const calculateTotalTTC = (cartItems) => {
+    const total = cartItems.reduce((accumulator, cartItem) => {
+      // Calculate the item's TTC price based on quantity
+      const itemPriceTTC = calculatePriceTTC(cartItem.product.price_excluding_tax, cartItem.product.tva);
+      const itemTotal = itemPriceTTC * cartItem.quantity;
+  
+      // Add the item's total to the overall total
+      return accumulator + itemTotal;
+    }, 0);
+  
+    // Round the total to 2 decimal places using toFixed
+    return Number(total.toFixed(2));
+  };
