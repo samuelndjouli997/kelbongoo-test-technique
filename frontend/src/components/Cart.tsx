216e03d18122756d5d64e2d67059c003c9b4a8b2
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 20:30:32 +0100

    added useEffect to update cart items number

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,11 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaCartArrowDown } from "react-icons/fa";
 import CartModal from './CartModal';
+import { CartItem } from '../types/types';
+import { useCart } from '../context/CartProvider';
 
 
 const Cart = () => {
 
   const [cartModalOpen, setCartModalOpen] = useState(false);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { state }:any = useCart();
+  const [totalCartItems, setTotalCartItems] = useState(0);
+
+  useEffect(() => {
+    // Calculate total cart items
+    const totalItems = state.cart.reduce((total:number, cartItem: CartItem) => total + cartItem.quantity, 0);
+    setTotalCartItems(totalItems);
+  }, [state.cart]);
+
   function openCartModal() {
     setCartModalOpen(true);
   }
@@ -14,7 +26,7 @@ const Cart = () => {
         <div className="text-primary-orange text-xl flex gap-x-2 items-center">
             {/* Cart */}
             <FaCartArrowDown className="cursor-pointer relative" onClick={openCartModal} />
-            <span className="font-bold">0</span> 
+            <span className="font-bold">{totalCartItems}</span> 
             {cartModalOpen && (
               <CartModal
                 cartModalOpen={cartModalOpen}
