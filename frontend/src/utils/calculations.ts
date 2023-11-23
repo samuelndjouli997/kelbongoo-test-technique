86c11923375b65d2758bee6ab03155a0614a7ec9
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 19:54:52 +0100

    added some type fixing

--- frontend/src/utils/calculations.ts
+++ frontend/src/utils/calculations.ts
@@ -1,3 +1,5 @@
+import { CartItem } from "../types/types";
+
 // Function to calculate the total price (TTC) including the tax 
 export const calculatePriceTTC = (priceHT: number, tva: number, quantity: number) => {
     const priceTTC = priceHT * (1 + tva / 100);
@@ -10,7 +12,7 @@ export const calculatePriceTTC = (priceHT: number, tva: number, quantity: number
   };
 
 // Function to calculate the total TTC for an cart
-export const calculateTotalTTC = (cartItems) => {
+export const calculateTotalTTC = (cartItems: CartItem[]):number => {
     const total = cartItems.reduce((accumulator, cartItem) => {
         // Here we use the calculatePriceTTC function 
         const itemPriceTTC = calculatePriceTTC(cartItem.product.price_excluding_tax, cartItem.product.tva, cartItem.quantity);
