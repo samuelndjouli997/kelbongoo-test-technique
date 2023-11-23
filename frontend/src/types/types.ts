3ba3c9706c72dd490ee54c14cec160c3118869fe
samuel_ndjouli <samuelndj.997@gmail.com>
2023-11-23 19:51:44 +0100

    added new types

--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -1,9 +1,38 @@
 export interface Product {
-    id: number;
+  id: number;
+  image: string;
+  name: string;
+  price_excluding_tax: number;
+  tva: number;
+  max_available_stock: number;
+  ordered_stock: number;
+}
+
+export interface CartItem {
+  product: {
     image: string;
     name: string;
     price_excluding_tax: number;
     tva: number;
-    max_available_stock: number;
-    ordered_stock: number;
-  }
+  };
+  quantity: number;
+}
+
+export interface CartState {
+  cart: CartItem[];
+  checked_out: boolean;
+  checked_out_products: CartItem[];
+}
+
+export interface CartAction {
+  type: string; 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  payload?: any;
+}
+
+export interface ProductItem {
+  product: {
+    id: number; 
+  };
+  quantity: number;
+}
